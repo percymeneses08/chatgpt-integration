5486e90baa958db3977019445be7e1660c81944d
percymeneses08 <percymeneses08@gmail.com>
2023-11-29 18:02:28 -0500

    last version working

--- scripts.js
+++ scripts.js
@@ -1,71 +1,56 @@
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
-const messages = document.getElementById('messages');
+const messageForm = document.getElementById("message-form")
+const messageInput = document.getElementById("message-input")
+const messages = document.getElementById("messages")
 
-function addMessage(content, user) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add(user);
-    messageElement.innerText = content;
-    messages.appendChild(messageElement);
-    messages.scrollTo(0, messages.scrollHeight);
+function addMessage(content, user)
+{
+  const messageElement = document.createElement("div")
+  messageElement.classList.add(user)
+  messageElement.innerText = content
+  messages.appendChild(messageElement)
+  messages.scrollTo(0, messages.scrollHeight)
 }
 
-/* VERSION 1.0 */
-// async function sendMessage(message) {
-//     try {
-//         const response = await fetch('/api/chatbot', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({ message })
-//         });
+async function sendMessage(message)
+{
+  try {
+    const context = "Eres un bot de respuestas. Tengo una página web dónde vas a responder cualquier pregunta relacionada a estos temas: Teléfono de la empresa: 123456789, procedimiento de pedido: delivery, tipos de productos: frutas, verduras, tubérculos, procedimiento de reclamo: a través de un correo interno."
+    const response = await fetch("/api/chatbot", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message, context })
+    });
 
-//         if (!response.ok) {
-//             throw new Error('Error en la respuesta');
-//         }
-
-//         const data = await response.json();
-//         addMessage(data.reply, 'bot');
-//     } catch (error) {
-//         console.error('Error al enviar el mensaje:', error);
-//     }
-// }
-/* end VERSION 1.0 */
-
-/* VERSION 1.5 */
-async function sendMessage(message) {
-    try {
-        const context = `Eres un bot de respuestas. Tengo una página web de _ llamada _ dónde vas a responder cualquier pregunta relacionada a estos temas: Teléfono de la empresa: 123456789, procedimiento de pedido: delivery, tipos de productos: frutas, verduras, tubérculos, procedimiento de reclamo: rápido y fácil.`;
-
-        const response = await fetch('/api/chatbot', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ message, context })
-        });
-
-        if (!response.ok) {
-            const errorText = await response.text();
-            throw new Error(`Error en la respuesta: ${response.status} - ${errorText}`);
-        }
-
-        const data = await response.json();
-        addMessage(data.reply, 'bot');
-    } catch (error) {
-        console.error('Error al enviar el mensaje:', error);
+    if (!response.ok)
+    {
+      const errorText = await response.text();
+      throw new Error("Error en la respuesta: " + response.status + " " + errorText);
     }
+    
+    const data = await response.json();
+    addMessage(data.reply, "bot");
+  }
+  catch (error)
+  {
+    console.error("Error al enviar el mensaje:", error);
+  }
 }
-/* end VERSION 1.5 */
 
-messageForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+messageForm.addEventListener("submit", handleSubmit)
 
-    const message = messageInput.value.trim();
-    if (!message) return;
-    addMessage(message, 'user');
-    messageInput.value = '';
+function handleSubmit(event)
+{
+  event.preventDefault()
+  const message = messageInput.value.trim()
 
-    sendMessage(message);
-});
+  if (!message) return
+
+  addMessage(message, "user")
+  messageInput.value = ""
+
+  sendMessage(message)
+}
+
+  
--- server.js
+++ server.js
@@ -1,96 +1,65 @@
-const express = require('express');
-const axios = require('axios');
-const dotenv = require('dotenv');
-const path = require('path');
-const openai = require('openai');
+const express = require("express")
+const axios = require("axios")
+const dotenv = require("dotenv")
+const path = require("path")
+const openai = require("openai")
 
-// Explicación: Importamos las librerías necesarias para construir la aplicación.
-dotenv.config();
+dotenv.config()
 
-// Explicación: Configuramos una aplicación Express, indicamos el uso de JSON y archivos estáticos.
-const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+const app = express()
+app.use(express.json())
+app.use(express.static(path.join(__dirname)))
 
-// Explicación: Configuramos una ruta para manejar peticiones GET a la raíz y enviar el archivo 'index.html'.
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+function handleGet(req, res)
+{
+  res.sendFile(path.join(__dirname, "index.html"))
+}
+app.get("/", handleGet)
 
-// Explicación: Configuramos la clave de la API de OpenAI utilizando la clave almacenada en las variables de entorno.
-openai.apiKey = process.env.OPENAI_API_KEY;
+openai.apiKey = process.env.OPENAI_API_KEY
 
-/* VERSION 1.0 solo respuestas de chatGTP */
-// app.post('/api/chatbot', async (req, res) => {
-//     const { message } = req.body;
+async function handlePost(req, res)
+{
+  try
+  {
+    const { message, context } = req.body
 
-//     try {
-//         const response = await axios.post(
-//             'https://api.openai.com/v1/completions',
-//             {
-//                 model: 'text-davinci-003',
-//                 prompt: message,
-//                 max_tokens: 100,
-//                 n: 1,
-//                 stop: null,
-//                 temperature: 0.8,
-//             },
-//             {
-//                 headers: {
-//                     'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-//                     'Content-Type': 'application/json'
-//                 }
-//             }
-//         );
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        messages: [
+          { role: "system", content: `${context}` },
+          { role: "user", content: `${message}` }
+        ],
+        model: "gpt-3.5-turbo",
+        max_tokens: 100,
+        n: 1,
+        temperature: 0.8,
+      },
+      {
+        headers: {
+          "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+          "Content-Type": "application/json"
+        }
+      }
+    )
 
-//         const reply = response.data.choices[0].text.trim();
-//         res.json({ reply });
+    const reply = response.data.choices[0].message.content.trim()
+    res.json({ reply })
 
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Error al procesar la solicitud' });
-//     }
-// });
-/* end VERSION 1.0 solo respuestas de chatGTP */
+  }
+  catch(error)
+  {
+    console.error(error);
+    res.status(500).json({ error: "Error al procesar la solicitud", details: error.message })
+  }
+}
+app.post('/api/chatbot', handlePost)
 
-/* VERSION 1.5 */
-/* Explicación: Configuramos una ruta para manejar peticiones POST a '/api/chatbot'.
-Utilizamos la API de OpenAI para obtener respuestas basadas en el mensaje y el contexto proporcionados.*/
-app.post('/api/chatbot', async (req, res) => {
-    try {
-        const { message, context } = req.body;
+const PORT = process.env.PORT || 3000
+function serverMessage()
+{
+  console.log("Servidor iniciado en el puerto " + PORT)
+}
 
-        const response = await axios.post(
-            'https://api.openai.com/v1/chat/completions',
-            {
-                messages: [
-                    { role: 'system', content: `${context}` },
-                    { role: 'user', content: `${message}` }
-                ],
-                model: 'gpt-4-vision-preview',
-                max_tokens: 100,
-                n: 1,
-                temperature: 0.8,
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-
-        const reply = response.data.choices[0].message.content.trim();
-        
-        res.json({ reply });
-
-    } catch (error) {
-        // console.error(error); <-- Más fácil de explicar para cursos
-        console.error(error.response ? error.response.data : error.message);
-        res.status(500).json({ error: 'Error al procesar la solicitud', details: error.message });
-    }
-});
-/* end VERSION 1.5 */
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
+app.listen(PORT, serverMessage)
